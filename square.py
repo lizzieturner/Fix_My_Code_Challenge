e203562af742eadd1ca737d91e1c8b69e6f86677
Lizzie Turner <hello@lizzieturner.com>
2018-09-04 23:43:21 +0000

    Fix square.py

--- square.py
+++ square.py
@@ -4,7 +4,6 @@ class square():
     
     width = 0
     height = 0
-
     
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
@@ -18,7 +17,7 @@ class square():
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.width)
 
 if __name__ == "__main__":
 
